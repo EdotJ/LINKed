a70cf2ed4b0370ebfa6e853325af235d74432b00
rob_mill <roberta.milasauskaite@gmail.com>
2019-11-10 18:01:52 +0200

    Add posts managing menu item and page

--- api/resources/views/layouts/app.blade.php
+++ api/resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('posts.index') }}">Manage Posts<span class="sr-only">(user posts)</span></a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
